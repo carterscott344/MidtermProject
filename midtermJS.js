00266c92ae0d06e9ddf15ec884c005fa2d156e7b
carterscott <98565111+carterscott344@users.noreply.github.com>
2024-10-16 19:27:56 -0500

    Delete midtermJS.js

--- midtermJS.js
+++
@@ -1,9 +0,0 @@
-fetch("./marioGames.json")
-    .then(response => response.json)
-    .then(games => loadGames(games))
-
-
-function loadGames(games){
-    const arrayGames = games;
-    console.log(arrayGames)
-}
