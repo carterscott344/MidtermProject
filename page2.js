8ffba2e5808f662a4d2e9d7be5a262b13b54297d
Nintencats73 <116706371+Nintencats73@users.noreply.github.com>
2024-10-19 16:00:30 -0500

    added stuff
    console and release date show up below when a game is clicked, along with a button which changes boxart and release date back and forth from japan and usa

--- page2.js
+++ page2.js
@@ -27,6 +27,12 @@ fetch('./data.json')
                 img.alt = `${game.title} Box Art`;
                 title.textContent = game.title;
 
+                
+
+                img.addEventListener('mouseover', function() {document.body.style.cursor = 'grab'});
+                img.addEventListener('mouseleave', function() {document.body.style.cursor = 'default'});
+                img.addEventListener('click', function() {showInfo(game, div, img)});
+
                 div.appendChild(img);
                 div.appendChild(title);
                 container.appendChild(div); // Append to the correct container
@@ -35,6 +41,74 @@ fetch('./data.json')
 
         displayGames(platformers, '#platformers');
         displayGames(partyGames, '#party');
-        displayGames(racingGames, '#race');
+        displayGames(racingGames, '#race');d
     })
     .catch(error => console.error('Error loading JSON data:', error));
+
+    let currentGame = "no";
+    let region = "USA";
+
+function showInfo(game, div, img) {
+
+    if (currentGame != "no") {
+        //document.getElementById("platform").remove();
+        //document.getElementById("release").remove();
+        //document.getElementById("change").remove();
+        document.getElementById("boxart").src = currentGame['USA-boxart'];
+        document.getElementById("boxart").id = 'none';
+        document.getElementById("subdiv").remove();
+        region = "USA";
+    } 
+    if (currentGame == game) {
+        currentGame = "no";
+    } else {
+
+        region = "USA";
+
+        const subdiv = document.createElement('div'); 
+        subdiv.id = 'subdiv';
+
+        const platform = document.createElement('p');
+        platform.textContent = game.platforms;
+        platform.id = 'platform';
+
+        const release = document.createElement('p');
+        release.textContent = game['USA-release-date'];
+        release.id = 'release';
+
+        const change = document.createElement('BUTTON');
+        change.textContent = "Show Japan data";
+        change.id = 'change';
+        change.addEventListener('click', function() {switchRegion(game, div, img)});
+
+        img.id = 'boxart';
+        
+    
+        subdiv.appendChild(platform);
+        subdiv.appendChild(release);
+        subdiv.appendChild(change);
+
+        div.appendChild(subdiv);
+    
+        currentGame = game;
+    }
+    
+
+}
+
+function switchRegion(game, div, img) {
+    
+    if (region == "USA") {
+        console.log("help");
+        img.src = game['Japan-boxart'];
+        release.textContent = game['Japan-release-date'];
+        change.textContent = "Show USA data";
+        region = "Japan";
+    } else {
+        console.log("no");
+        img.src = game['USA-boxart'];
+        release.textContent = game['USA-release-date'];
+        change.textContent = "Show Japan data";
+        region = "USA";
+    }
+}
